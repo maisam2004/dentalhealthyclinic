a6b6dd263fb3defa5435cf63b82757a605745d0e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-07-02 21:08:38 +0100

    Update settings.py for email host

--- dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/settings.py
@@ -105,7 +105,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True 
 EMAIL_HOST_USER = 'maisam2004@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'uajc fert vysn pbvu'  # Your Gmail password (or app password)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # Your Gmail password (or app password)
 
 
 
